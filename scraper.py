6f8f167e8cb39dae3baa788fb9033c5625e77a5e
nikkar1985 <nkarountzos.ka@iekdelta360.edu.gr>
2026-02-05 17:43:57 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,20 +1,19 @@
 import yfinance as yf
 import json
 
-#Λίστα με τα σύμβολα που θα θέλουμε
+# Λίστα με τα σύμβολα που θέλεις
+tickers = ["BTC-USD", "ETH-USD", "AAPL", "TSLA"]
 
-tickers=["BTC-USD", "ETH-USD", "AAPL", "TSLA"]
-
-data_to_save={}
-
+data_to_save = {}
 
 for symbol in tickers:
-    ticker=yf.Ticker(symbol)
+    ticker = yf.Ticker(symbol)
+    # Παίρνουμε την τελευταία τιμή
     price = ticker.fast_info['last_price']
-    data_to_save[symbol] = round(price,2)
+    data_to_save[symbol] = round(price, 2)
 
+# Αποθήκευση σε JSON αρχείο για να το διαβάσει η HTML
+with open('data.json', 'w') as f:
+    json.dump(data_to_save, f)
 
-with open('data.json, 'w') as f:
-    json.dumo(data_to_save,f)          
-
-print("Data updated successfully")
+print("Data updated successfully!")
